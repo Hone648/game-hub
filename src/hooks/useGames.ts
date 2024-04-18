988b45b6f2802ed2192f995fe072f652e8a722f0
Hunter Kam <hone648@gmail.com>
2024-04-18 11:07:00 +0900

    display platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
