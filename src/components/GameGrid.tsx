8380e4f6f8c7ffabdd924dfdde596b6c09c86d5f
Hunter Kam <hone648@gmail.com>
2024-04-23 10:14:23 +0900

    refactor data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSketeton from './GameCardSketeton';
 import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -22,7 +22,7 @@ const GameGrid = () => {
               <GameCardSketeton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
